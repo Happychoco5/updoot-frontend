53f9755165e491efb2fe951bce0b762e1a0dd292
Kurtis <kreid8@kent.edu>
2022-10-20 15:04:40 -0400

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CreatePostsComponent } from './components/create-posts/create-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CreatePostsComponent
   ],
   imports: [
     BrowserModule,
