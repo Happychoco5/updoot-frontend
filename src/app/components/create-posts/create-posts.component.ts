d1a95f04d06ca008817ebf12ac47e65315904b00
Kurtis <kreid8@kent.edu>
2022-10-21 14:04:58 -0400

    added form to create post

--- src/app/components/create-posts/create-posts.component.ts
+++ src/app/components/create-posts/create-posts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Thread } from 'src/app/models/thread/thread';
+import { PostService } from 'src/app/services/post/post.service';
+
 
 @Component({
   selector: 'app-create-posts',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postService: PostService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  registerForm!: FormGroup;
+  submitted = false;
+
+  /*
+        public thread_id: number,
+        public account_id: number,
+        public title: string,
+        public content: string,
+        public epoch: number,
+        public updoot: number
+  */
+  async onSubmit(){
+    const thread: Thread = {thread_id: 0,
+      account_id:this.registerForm.controls['account_id'].value,
+      title:this.registerForm.controls['title'].value,
+      content:this.registerForm.controls['comment'].value,
+      epoch:0,
+      updoot: 0};
+      this.createPost(thread);
+  }
+  
+
+  async createPost(thread: Thread)
+  {
+    this.postService.makeSingleThread(thread);
   }
 
 }
