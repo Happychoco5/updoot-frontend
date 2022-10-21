0a656c68e1bc9138f894b01ee519bce5d9257f0a
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-10-21 10:01:34 -0700

    reply services work

--- src/app/components/reply/reply.component.ts
+++ src/app/components/reply/reply.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Reply } from 'src/app/models/reply/reply';
+import { ReplyService } from 'src/app/services/reply/reply.service';
+
 
 @Component({
   selector: 'app-reply',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private replyService: ReplyService) { }
 
   ngOnInit(): void {
+    this.createReplyForPost();
+    this.getRepliesByPost();
+  }
+  getRepliesByPost(){
+    this.replyService.getRepliesForPost(1).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
   }
 
+  createReplyForPost(){
+    const reply= new Reply(0, 1, 1, "...", 0, 0);
+    this.replyService.registerPostReply(reply).subscribe(
+      (res) => {
+        console.log(res);
+      }, 
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
