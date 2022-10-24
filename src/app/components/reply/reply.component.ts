148c31dd4861c945331d3a9eb623af6d4e9677fa
Timothy Wu <twu23@buffalo.edu>
2022-10-24 17:25:50 -0400

    updooting functionality partially there

--- src/app/components/reply/reply.component.ts
+++ src/app/components/reply/reply.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Reply } from 'src/app/models/reply/reply';
+import { UpdootedReply } from 'src/app/models/updooted/updooted-reply';
 import { ReplyService } from 'src/app/services/reply/reply.service';
+import { UpdootService } from 'src/app/services/updoot/updoot.service';
 
 
 @Component({
@@ -12,19 +14,24 @@ export class ReplyComponent implements OnInit {
   @Input() postId!: number;
   replies: Reply[] = [];
   replyContent: string = "";
+  updooted: boolean = false;
 
-  constructor(private replyService: ReplyService) {}
+  constructor(private replyService: ReplyService, private updootService: UpdootService) {}
 
   ngOnInit(): void {
     // this.createReplyForPost();
     // this.getRepliesByPost();
     this.getRepliesByPost(this.postId);
   }
+  
   getRepliesByPost(postId:number){
     this.replyService.getRepliesForPost(postId).subscribe(
       {
         next: (replies) => {
         this.replies = replies;
+        for(let reply of this.replies){
+          this.checkForUpdoot(reply.replyId);
+        }
         },
         error: (err) => {
         console.log(err)
@@ -47,6 +54,33 @@ export class ReplyComponent implements OnInit {
 
   getTimestampInSeconds () {
     return Math.floor(new Date().getTime()/1000.0);
+  }
+
+  updootReply(reply: Reply){
+    const updootedReply = new UpdootedReply(0, 1, reply.replyId);
+    this.updootService.postUpdootReply(updootedReply).subscribe(
+      {next: (res) => {
+        this.updootService.updootReply(reply.replyId).subscribe({
+          next: (res2) => {
+            reply.updoot = res2;
+          },
+          error: (err2) => {
+            console.log(err2);
+          }
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  checkForUpdoot(replyId: number){
+    this.updootService.getUpdootedReply(1, replyId).subscribe({
+      next: (res) => {
+        return res;
+      }
+    })
   }
 
 }
