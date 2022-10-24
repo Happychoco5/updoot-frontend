d24dd16ef02459d3458dec24fd9d5bb8d6d61ce0
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-10-24 09:26:41 -0700

    display replies

--- src/app/components/reply/reply.component.ts
+++ src/app/components/reply/reply.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { Reply } from 'src/app/models/reply/reply';
+import { ReplyService } from 'src/app/services/reply/reply.service';
+
 
 @Component({
   selector: 'app-reply',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reply.component.css']
 })
 export class ReplyComponent implements OnInit {
+  @Input() postId!: number;
+  replies: Reply[] = [];
 
-  constructor() { }
+  constructor(private replyService: ReplyService) {}
 
   ngOnInit(): void {
+    // this.createReplyForPost();
+    // this.getRepliesByPost();
+    this.getRepliesByPost(this.postId);
+  }
+  getRepliesByPost(postId:number){
+    this.replyService.getRepliesForPost(postId).subscribe(
+      {
+        next: (replies) => {
+        this.replies = replies;
+        },
+        error: (err) => {
+        console.log(err)
+        }
+    });
   }
 
 }
