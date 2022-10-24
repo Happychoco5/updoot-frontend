d437233373a9b5c94af2243bf607ff92a39f63e0
Alberto Gutierrez <albertogutierrez700@gmail.com>
2022-10-24 14:09:46 -0400

    added navbar stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostsComponent } from './components/create-posts/create-posts.component';
+import { PostsComponent } from './components/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:PostsComponent},
+  {path:"create-posts", component:CreatePostsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
